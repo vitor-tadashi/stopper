5aa4c2a23158b31d9f5ab97ea27ac791a4e66bec
VERITY\guilherme.oliveira <guilherme.oliveira@verity.com.br>
2017-09-08 14:22:30 -0300

    alterações de multi-empresa!

---
+++ pause-parent/pause-web/src/main/java/br/com/verity/pause/controller/MultiEmpresaController.java
@@ -0,0 +1,45 @@
+package br.com.verity.pause.controller;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.verity.pause.bean.FuncionalidadeBean;
+import br.com.verity.pause.bean.UsuarioBean;
+
+@Controller
+public class MultiEmpresaController {
+
+	@RequestMapping(value = { "", "/", "/selecionaMultiEmpresa" }, method = RequestMethod.GET)
+	public String multiEmpresa(Model model, RedirectAttributes redirect) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		UsuarioBean usuarioLogado = (UsuarioBean) auth.getPrincipal();
+		for (FuncionalidadeBean funcionalidade : usuarioLogado.getPerfis().get(0).getFuncionalidades()) {
+			if (funcionalidade.getNome().equalsIgnoreCase("MultiEmpresa")
+					|| funcionalidade.getNome().equalsIgnoreCase("Multi-Empresa")
+					|| funcionalidade.getNome().equalsIgnoreCase("Multi Empresa")) {
+				return "multi-empresa/multi-empresa";
+			}
+		}
+		return "redirect:/consultar-apontamento";
+	}
+
+	@RequestMapping(value = { "/selecionaMultiEmpresa/{id}" }, method = RequestMethod.POST)
+	public String selecionaMultiEmpresa(@PathVariable Integer id, RedirectAttributes redirect) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		UsuarioBean usuarioLogado = (UsuarioBean) auth.getPrincipal();
+		
+		usuarioLogado.setIdEmpresaSessao(id);
+
+		Authentication newAuth = new UsernamePasswordAuthenticationToken(usuarioLogado, usuarioLogado.getSenha(), auth.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(newAuth);
+
+		return "redirect:/importacao";
+	}
+}
