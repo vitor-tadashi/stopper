392f5a8513abc308260827b59b6da7975f91f297
amauri.souza <amauri.souza@verity.com.br>
2018-01-08 09:23:26 -0200

    Alterações para enviar a despesa para o controller

---
+++ src/pause-model/src/main/java/br/com/verity/pause/bean/DespesaBean.java
@@ -0,0 +1,53 @@
+package br.com.verity.pause.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class DespesaBean implements Serializable {
+	
+	private Date data;
+	private Double valor;
+	private Long tipoDespesa;
+	private Long centroCusto;
+	private String justificativa;
+	private MultipartFile comprovante;
+	
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public Long getTipoDespesa() {
+		return tipoDespesa;
+	}
+	public void setTipoDespesa(Long tipoDespesa) {
+		this.tipoDespesa = tipoDespesa;
+	}
+	public Long getCentroCusto() {
+		return centroCusto;
+	}
+	public void setCentroCusto(Long centroCusto) {
+		this.centroCusto = centroCusto;
+	}
+	public String getJustificativa() {
+		return justificativa;
+	}
+	public void setJustificativa(String justificativa) {
+		this.justificativa = justificativa;
+	}
+	public MultipartFile getComprovante() {
+		return comprovante;
+	}
+	public void setComprovante(MultipartFile comprovante) {
+		this.comprovante = comprovante;
+	}
+}
--- src/pause-web/src/main/java/br/com/verity/pause/controller/DespesaController.java
+++ src/pause-web/src/main/java/br/com/verity/pause/controller/DespesaController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.verity.pause.bean.DespesaBean;
 import br.com.verity.pause.business.TipoDespesaBusiness;
 
 @Controller
@@ -27,7 +29,7 @@ public class DespesaController {
 	
 	@RequestMapping(value = "", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<?> salvar() {
+	public ResponseEntity<?> salvar(@RequestBody DespesaBean despesa) {
 		
 		return ResponseEntity.ok("Salvo com sucesso!");
 	}
